769568fd23d8816eb0abfad6a81b6ad545bd03d2
vizam <vizamdevelop@gmail.com>
2016-09-07 14:04:13 -0430

    El panel de respaldo y las funciones asociadas trabajan. La copia de la DB produce un error, la primera vez con poca informacion, la sobreescritura da otro error con mas detalles. Habla de archivo LOCK ,el cual esta en la carpeta que termina en leveldb. Pero al traer de vuelta manualmente la carpeta, esta toda la informacion

--- respaldo.js
+++ respaldo.js
@@ -1,24 +1,26 @@
 
+
 var fs = require('fs-extra');
-var homedrive = process.env.HOMEDRIVE; 
-var datapath = nw.App.dataPath;      //"C:\Users\Ito\AppData\Local\medih\User Data\Default"
+var path = require('path');
+var homedrive = process.env.HOMEDRIVE; //"C:"
 var homepath = process.env.HOMEPATH; //"C:\Users\Ito"
-var winpath = "\\IndexedDB";
+var datapath = nw.App.dataPath;      //"C:\Users\Ito\AppData\Local\medih\User Data\Default" here \IndexedDB is required
+var carpetaidb = path.sep + "IndexedDB"; //fs.copy for directory ignore parent directory
 
 function panelRespaldo() {
-  chrome.storage.local.get(['proveedor', 'path'], (objeto) => { 
+  chrome.storage.local.get(['proveedor', 'ruta'], (objeto) => { 
     var proveedor = objeto.proveedor;
-    var path = objeto.path;
+    var ruta = objeto.ruta;
     var forma = document.getElementById('formaRespaldo');
     var cintillo = document.getElementById('infoRespaldo'); // input type text element from formaRespaldo
     if (proveedor && proveedor != 'Local') {
-      cintillo.value =  homedrive + homepath + '\\' + proveedor; //: chrome.i18n.getMessage('defecto_opcionnoasignada');
+      cintillo.value =  homedrive + homepath + path.sep + proveedor; //: chrome.i18n.getMessage('defecto_opcionnoasignada');
       cintillo.style.color = 'Green';
     } else if (!proveedor) {
       cintillo.value =  chrome.i18n.getMessage('defecto_opcionnoasignada');
       cintillo.style.color = 'Red';
     } else {
-      cintillo.value = path;
+      cintillo.value = ruta;
       cintillo.style.color = 'Green';
     }
     for (var x of forma.elements) {
@@ -29,7 +31,7 @@ function panelRespaldo() {
   });
   function evaluarProveedor(x, proveedor) {
     if (x.dataset.proveedor != 'Local') {
-      fs.stat(homepath + "\\" + x.dataset.proveedor, (err, stats) => { //stats is fs.Stats object
+      fs.stat(homepath + "/" + x.dataset.proveedor, (err, stats) => { //stats is fs.Stats object
         if (err) {
           console.log(x.dataset.proveedor + ' no instalado');
         } else {
@@ -45,49 +47,69 @@ function panelRespaldo() {
 }
 
 document.getElementById('botonLocal').addEventListener('click', (evt) => {
-  var selector = document.getElementById('dialogoEscondido');
+  var input = document.getElementById('inputEscondido');
   var cintillo = document.getElementById('infoRespaldo'); 
-  selector.addEventListener('change', (evt) => {
-    if (chrome.runtime.lastError) return;
-      console.log(selector.files); //this files object only has one key/value, value is another object with properties
-      chrome.storage.local.set({path: selector.files[0].path}, () => {   //function is optional, nothing passed, 
-        if (chrome.runtime.lastError) {
-          console.log('se produjo un error');//  if error, chrome.runtime.lastError is set
-        } else {
-          cintillo.value = selector.files[0].path;
-          cintillo.style.color = 'Black';
-        }
-      });
+  input.addEventListener('change', (evt) => {
+    cintillo.value = input.files[0].path;
+    cintillo.style.color = 'Black';
   });
-  selector.click();
+  input.click();
 });
 
-document.getElementById('preferenciaRespaldo').addEventListener('click', (evt) => { 
-  var opciones = document.getElementsByName('proveedor');
-  opciones.forEach((elemento) => { //custom function for like Array dom element
-    if (elemento.checked == true) {
-      chrome.storage.local.set({proveedor: elemento.dataset.proveedor}, function () {
-        mensajeEstado(chrome.i18n.getMessage('mensaje_preferenciagrabada'));
-      });
-      panelRespaldo();
+document.getElementById('preferenciaRespaldo').addEventListener('click', configuracionRespaldo);
+function configuracionRespaldo() {
+  var selecciones = document.getElementsByName('proveedor'); //input type radio with name='proveedor'
+  var elemento = document.getElementById('inputEscondido');
+  var objeto = document.getElementById('inputEscondido').files[0]; // return FileList object
+  var cintillo = document.getElementById('infoRespaldo');
+  var directorio;
+  for (var x of selecciones) {
+    if (x.dataset.proveedor && x.checked == true) {
+      directorio = (objeto) ? objeto.path : homedrive + homepath + x.dataset.proveedor;
+      guardar(x);
       return;
-    } 
-    mensajeEstado(chrome.i18n.getMessage('mensaje_nadaseleccionado'));
-  })
-})
+    }
+  }
+  mensajeEstado(chrome.i18n.getMessage('mensaje_nadaseleccionado'));
+  function guardar(x) {
+    chrome.storage.local.set({proveedor: x.dataset.proveedor, ruta: directorio}, () => {
+      if (chrome.runtime.lastError) { 
+        console.log('se produjo un error'); //if error, lastError is set
+      } else {
+        cintillo.value = (directorio) ? directorio : homedrive + homepath + path.sep + x.dataset.proveedor;
+        cintillo.style.color = 'Green'; 
+        elemento.value = null; //reset files object in case further selections are made/needed
+        mensajeEstado(chrome.i18n.getMessage('mensaje_preferenciagrabada'));
+      }
+    }); 
+  }
+}
+
 document.getElementById('botonRespaldar').addEventListener('click', (evt) => {
-  fs.mkdirs(homepath + winpath, (err) => {
-    if (err) console.error(err);
-    console.log('Success');
+  fs.mkdirs(homepath + carpetaidb, (err) => {
+    if (err) {
+      console.log('error al crear directorio');
+      console.error(err);
+      return;
+    }
+    console.log('Success creating/checking directory');
+    chrome.storage.local.get('ruta', (objeto) => {
+      if (chrome.runtime.lastError) {
+        console.log('se produjo un error, runtime.lastError');
+      } else {
+        var ruta = objeto.ruta;
+        fs.copy(datapath + carpetaidb, ruta + carpetaidb, //It does not include the parent directory source
+          { clobber:true,  
+            dereference:true,
+            preserveTimestamps:false, 
+            filter:filtro
+          }, (err) => {//Next function trow error (some files not copied) but both directories seems to be the same
+              if (err) return console.error(err);
+              console.log('copia exitosa');
+        });
+      }
+    });
   });
-  // Next function trow error (some files not copied) but both directories seems to be the same
-  fs.copy(datapath + winpath, homepath + winpath, { clobber:true,  //It does not include the parent directory source
-                                dereference:true,
-                                preserveTimestamps:false, 
-                                filter:filtro
-                              }, (err) => {
-                                if (err) console.error(err);
-                              });
   // For sudden filter needs
   function filtro() {
     return true;
